7d0a2878a423cb4c33adda0864d3e52311938779
Muhammadjon Abduvahobov <115058559+Megacoderuzb@users.noreply.github.com>
2023-03-02 21:37:16 +0500

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "cedf90b96dmsha4688a1d81e00d7p1b98e0jsn6a4df27afbd3",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
